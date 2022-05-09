022758b85c88e7c2d51ffc3637aba45c4831bb5b
Mohamed Bengrich <77544040+MohamedBng@users.noreply.github.com>
2022-05-09 18:55:00 +0200

    Add files via upload

---
+++ date.php
@@ -0,0 +1,99 @@
+<?php 
+require_once('autoload.php');
+$date = new Reservation;
+$date->selectDate();
+$dates = $date->getDate();
+?>
+<!DOCTYPE html>
+<html lang="en"><!-- Basic -->
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">   
+   
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+     <!-- Site Metas -->
+    <title>Yamifood Restaurant - Responsive HTML5 Template</title>  
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Site Icons -->
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">    
+	<!-- Site CSS -->
+    <link rel="stylesheet" href="css/style.css">       
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/custom.css">
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+	<!-- Start Reservation -->
+	<form action="time.php" method="post">
+	<div class="reservation-box">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="heading-title text-center">
+						<h2>Reservation</h2>
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting</p>
+					</div>
+				</div>
+			</div>
+										<div class="form-group">
+											<input id="input_date" class="dateTimepicker picker__input form-control" name="dateUser" type="text"  placeholder="Selectionner une date" equired data-error="Please enter Date">
+											<div class="help-block with-errors"></div>
+										</div>
+										<div class="submit-button text-center">
+										<button class="btn btn-common" id="submit" type="submit">Book Table</button>
+										<div id="msgSubmit" class="h3 text-center hidden"></div> 
+										<div class="clearfix"></div> 
+									</div>                                
+									</div>                                 
+							
+	</form>
+	<a href="#" id="back-to-top" title="Back to top" style="display: none;">&uarr;</a>
+
+	<!-- ALL JS FILES -->
+
+  <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+	<script src="js/bootstrap.min.js"></script>
+    <!-- ALL PLUGINS -->
+	
+
+
+  
+<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+    <script type="text/javascript">
+
+
+<?php 
+echo "tab = '".implode("<>", $dates)."'.split('<>');"; ?>
+
+
+flatpickr(".dateTimepicker", {
+  minDate: "today",
+  enable: tab
+  
+});
+
+console.log(tab);
+
+
+
+</script>
+</body>
+</html>
